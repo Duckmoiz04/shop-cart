e82771c6ed22a3bb1d7fcfd37b80f1ea43498307
duckmoiz040 <dqviet204@gmail.com>
2025-08-06 12:36:25 +0700

    Create Project

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Footer = () => {
+  return (
+    <footer className="bg-red-300">
+      <Container>
+        Footer
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from './Container'
+import Logo from './Logo'
+
+const Header = () => {
+  return (
+    <header className="">
+      <Container>
+        {/* Logo */}
+        <Logo />
+
+        {/* Main navigation */}
+
+        {/* User utilities */}
+      </Container>
+    </header>
+  )
+}
+
+export default Header
